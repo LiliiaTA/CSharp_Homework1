fd7f0b4986380742a480d1df436d1cea0b4e0f33
Liliia Tliasheva <liliialbertovn@gmail.com>
2023-02-14 04:47:49 +0700

    Task35

---
+++ Lesson5_Tasks35,37/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 35: Задайте одномерный массив из 123 случайных чисел [0: 1000].
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+*/
+
+Console.Clear();
+int[] array = GetRandomArray();
+Console.WriteLine($"Массив из 123 элементов: [{String.Join(", ", array)}]");
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10,99]: {CountEl(array)}");
+
+int[] GetRandomArray(){
+    int size = 123;
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++){
+        result[i] = new Random().Next(0, 1000);
+    }
+    return result;
+}
+
+int CountEl(int[] arr){
+    int count = 0;
+    foreach(int el in arr){
+        if (10 <= el && el <= 99){
+            count++;
+        }
+    }
+    return count;
+    
+}
